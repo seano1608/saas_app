2711cec7896013f509a5f99681e402b98cbaeee1
Sean Thomson <seantomo1680@gmail.com>
2025-05-29 15:10:34 +0200

    Added all routes

---
+++ app/companions/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const CompanionSession = () => {
+    return <div>Session</div>
+}
+
+export default CompanionSession
---
+++ app/companions/new/page.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const NewCompanion = () => {
+    return <div>New</div>
+}
+
+export default NewCompanion
