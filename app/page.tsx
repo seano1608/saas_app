70c902248967e852b89f8825398cd74ac067fd8f
Sean Thomson <seantomo1680@gmail.com>
2025-06-05 19:23:45 +0200

    Built Home Page and Companion Form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,51 @@
 import React from "react"
+import CompanionCard from "@/components/CompanionCard"
+import CompanionsList from "@/components/CompanionsList"
+import CTA from "@/components/CTA"
+import { recentSessions } from "@/constants"
 
 const Page = () => {
-    return <div>Home</div>
+    return (
+        <main>
+            <h1>Popular Companions</h1>
+
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="Neura the Brainy Explorer"
+                    topic="Neural Network of the Brain"
+                    subject="science"
+                    duration={45}
+                    color="#E5D0FF"
+                />
+                <CompanionCard
+                    id="456"
+                    name="Countsy the Number Wizard"
+                    topic="Derivatives & Integrals"
+                    subject="maths"
+                    duration={30}
+                    color="#FFDA6E"
+                />
+                <CompanionCard
+                    id="789"
+                    name="Verba the Vocabulary Builder"
+                    topic="English Literature "
+                    subject="language"
+                    duration={30}
+                    color="#BDE7FF"
+                />
+            </section>
+
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently Completed Sessions"
+                    companions={recentSessions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA />
+            </section>
+        </main>
+    )
 }
 
 export default Page
